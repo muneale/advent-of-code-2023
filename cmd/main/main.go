d804105a665f29f6003aa229a61f19e7d8a91d7a
Alessio <munerato.ale@gmail.com>
2023-12-02 17:04:28 +0100

    Parametrized main by accepting input arguments.

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -6,10 +6,25 @@ import (
 	"strings"
 
 	"github.com/muneale/advent-of-code-2023/internal/n01"
+	"github.com/muneale/advent-of-code-2023/internal/n02"
 )
 
 func main() {
 
+	day := os.Args[1]
+
+	switch day {
+	case "01":
+		runDay01()
+	case "02":
+		runDay02()
+	default:
+		fmt.Printf("Invalid day: %s\n", day)
+	}
+
+}
+
+func runDay01() {
 	input, _ := os.ReadFile("./input/n01.txt")
 	data := strings.Split(string(input), "\n")
 
@@ -20,4 +35,25 @@ func main() {
 	}
 
 	fmt.Printf("Part 2: %d\n", n01.GetTotalCalibrationValue(data))
+}
+
+func runDay02() {
+
+	input, _ := os.ReadFile("./input/n02.txt")
+
+	games := n02.ParseGames(string(input))
+
+	maxCubes := n02.Cubes{
+		Blue:  14,
+		Green: 13,
+		Red:   12,
+	}
+
+	sum := n02.GetSumOfPossibleCombinations(maxCubes, games)
+
+	fmt.Printf("Part 1: %d\n", sum)
+
+	sum = n02.GetSumOfPowerMinimumCombinations(games)
+
+	fmt.Printf("Part 2: %d\n", sum)
 }
